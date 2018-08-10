e32ffa3516a16c645f9de9df7e0d29574990b020
CarolinaMisa <1700250@ucc.edu.ar>
2018-08-10 16:12:06 -0300

    Ejercicio 2

--- U00_Repaso/Ej-02/Calculadora.h
+++ U00_Repaso/Ej-02/Calculadora.h
@@ -1,9 +1,27 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
+#include <iostream>
 
+using namespace std;
 
+template <class T>
 class Calculadora {
+public:
+    T sumar (T a , T b){
+        return a + b;
+    }
 
+    T restar (T a , T b){
+        return a - b;
+    }
+
+    T multiplicar (T a , T b){
+        return a * b;
+    }
+
+    T dividir (T a , T b){
+        return a / b;
+    }
 };
 
 
--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "Calculadora.h"
-
 using namespace std;
 
 int main() {
+    Calculadora <int> micalc;
+    Calculadora <double> micalcdouble;
+
     cout << "Ejercicio 00/02\n" << endl;
+
+    cout << micalc.sumar(2,2) << endl;
+    cout << micalc.restar(2,3) << endl;
+    cout << micalc.dividir(4,2) << endl;
+    cout << micalc.multiplicar(5,2) << endl;
+
+    cout << micalcdouble.sumar(2.5,2) << endl;
+    cout << micalcdouble.restar(2,1.5) << endl;
+    cout << micalcdouble.dividir(1,2) << endl;
+    cout << micalcdouble.multiplicar(1.5,2) << endl;
+
     return 0;
 }
