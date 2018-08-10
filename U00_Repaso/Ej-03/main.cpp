fab23226eb853f13c1dec205b9d57a9e86693483
CarolinaMisa <1700250@ucc.edu.ar>
2018-08-10 17:17:17 -0300

    Ejercicio 3 - Excepciones

--- U00_Repaso/Ej-03/main.cpp
+++ U00_Repaso/Ej-03/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include "Calculadora.h"
-
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/03\n" << endl;
+    Calculadora <int> micalc;
+    Calculadora <double> micalcdouble;
+
+    cout << "Ejercicio 00/02\n" << endl;
+
+    cout << micalc.sumar(2,2) << endl;
+    cout << micalc.restar(2,3) << endl;
+    cout << micalc.dividir(4,2) << endl;
+    cout << micalc.multiplicar(5,2) << endl;
+
+    cout << micalcdouble.sumar(2.5,2) << endl;
+    cout << micalcdouble.restar(2,1.5) << endl;
+    cout << micalcdouble.dividir(1,2) << endl;
+    cout << micalcdouble.multiplicar(1.5,2) << endl;
+
+    try {
+        cout << micalc.dividir(4,0) << endl;
+    }   catch (int e){
+        cout << "error: " << e <<endl;
+    }
+
     return 0;
 }
