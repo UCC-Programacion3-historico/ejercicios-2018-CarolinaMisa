ac43c55f3aec19918a2c998692d57f65a2bda37e
CarolinaMisa <1700250@ucc.edu.ar>
2018-08-21 20:13:38 -0300

    terminar sumatoria

--- U01_Recursividad/Ej-02/main.cpp
+++ U01_Recursividad/Ej-02/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "sumatoria.h"
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 01/02\n" << std::endl;
+
+    cout << "Ejercicio 01/02\n" << endl;
+    int array[] = { 1, 2 , 3 , 4 };
+    cout << sumatoria(array,4);
+
     return 0;
 }
