165eafaa9226a5ad311e178a66b0ac1a0af06dc9
CarolinaMisa <1700250@ucc.edu.ar>
2018-08-21 19:58:56 -0300

    Ejercicio 1 potencia

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "potencia.h"
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 01/01\n" << std::endl;
+    cout << "Ejercicio 01/01\n" << endl;
+    cout << potencia(2,2) << endl;
+    cout << potencia(5,12) << endl;
+    cout << potencia(30,2) << endl;
+
     return 0;
 }
